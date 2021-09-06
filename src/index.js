b724e116bb16a18173b50e8aacff554387c45890
Erick <erickorso@gmail.com>
2021-09-06 18:45:32 -0300

    saga-files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { Provider } from 'react-redux';
+import { BrowserRouter, Switch } from 'react-router-dom';
+
+import RouteApp from './components/App';
+import getStore from './store/getStore';
+
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Users from './components/Users';
+import Counter from './components/Counter';
+import Home from './components/Home';
+
+const { store } = getStore();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <Switch>
+        <RouteApp path={'/users'} component={Users} />
+        <RouteApp path={'/counter'} component={Counter} />
+        <RouteApp path={'/'} component={Home} />
+      </Switch>
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
